9037dc4f7c337661d9901bb76e5fa8185090a016
AnastasiaU <anastasia.trembach@gmail.com>
2020-01-02 14:33:29 +0000

    labs are done

--- Exercises/1-seq.js
+++ Exercises/1-seq.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const seq = f => g => x => 0;
+const seq = f => g => {
+  if (typeof g === 'number') return f(g);
+  return seq(x => f(g(x)));
+};
+
+// seq(x => x + 1)
+//    (x => x * 2)
+//    (x => x / 3)
+//    (x => x - 4)(7);
 
 module.exports = { seq };
--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,28 @@
 'use strict';
 
-const array = () => null;
+const array = () => {
+	const arr = [];
+	const fn = i => arr[i];
+	fn.push = x => arr.push(x);
+	fn.pop = () => arr.pop();
+	return fn;
+};
+
+
+const arr = array();
+
+arr.push('first');
+arr.push('second');
+arr.push('third');
+
+console.log(arr(0)); // Выводит: first
+console.log(arr(1)); // Выводит: second
+console.log(arr(2)); // Выводит: third
+
+console.log(arr.pop()); // Выводит: third
+console.log(arr.pop()); // Выводит: second
+console.log(arr.pop()); // Выводит: first
+
+console.log(arr.pop()); // Выводит: undefined
 
 module.exports = { array };
